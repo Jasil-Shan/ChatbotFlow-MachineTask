49137204f259036e86a580ed396bc7e2afa02963
Jasil Shan <jasilshan786@gmail.com>
2024-02-26 03:47:29 +0530

    Button added

---
+++ src/components/NavBar/SaveButton/SaveButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SaveButton = () => {
+  return <button className='border-2 rounded-md border-blue-900 bg-slate-200 px-5 py-1 mr-16 font-semibold'>Save Changes</button>
+}
+
+export default SaveButton
