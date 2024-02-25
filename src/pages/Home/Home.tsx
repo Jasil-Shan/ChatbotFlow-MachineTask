d0586595acacd2fbfdb360bf083eab96f0939001
Jasil Shan <jasilshan786@gmail.com>
2024-02-26 03:25:29 +0530

    Ui fix

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import Flow from "../../components/Flow/Flow";
+
+const Home = () => {
+  return (
+    <>
+    <div style={{ height: "100vh" , width:'100%'}}>
+      <Flow />
+    </div>
+    </>
+  );
+};
+
+export default Home;
