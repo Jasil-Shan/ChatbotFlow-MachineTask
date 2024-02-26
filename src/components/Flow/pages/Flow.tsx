7c98ad7449ea3a16a3dc67c9cc4a5c85f818e51c
Jasil Shan <jasilshan786@gmail.com>
2024-02-26 15:31:14 +0530

    changed into a page and added validation for edges

---
+++ src/components/Flow/pages/Flow.tsx
@@ -0,0 +1,134 @@
+import React, { useState, useRef, useCallback } from "react";
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+} from "reactflow";
+import "reactflow/dist/style.css";
+import SidePanel from "../../SidePanel/SidePanel";
+import TextNode from "../Custom/TextNode/TextNode";
+import Navbar from "../../NavBar/pages/Navbar";
+
+let id = 0;
+const getId = () => `${id++}`;
+
+const nodeTypes = {
+  custom: TextNode,
+};
+
+const Flow = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [selectedNode, setSelectedNode] = useState(null);
+  const [editedNode, setEditedNode] = useState();
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
+
+  const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
+
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData("application/reactflow");
+
+      // check if the dropped element is valid
+      if (typeof type === "undefined" || !type) {
+        return;
+      }
+
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type: "custom",
+        position,
+        data: { label: `${type} ${id}` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance]
+  );
+
+  const onNodeClick = useCallback((event: React.MouseEvent, node: Node) => {
+    setSelectedNode(node);
+  }, []);
+
+  const saveEditedNode = useCallback(() => {
+    setNodes((prevNodes) =>
+      prevNodes.map((node) =>
+        node.id === selectedNode.id
+          ? { ...node, data: { ...node.data, label: editedNode } }
+          : node
+      )
+    );
+  }, [selectedNode, setNodes, editedNode]);
+
+  const handleEdit = (newText) => {
+    setEditedNode(newText);
+  };
+
+  const isValidConnection = useCallback(
+    (connection) => {
+      const { source, target } = connection;
+      const sourceEdges = edges.filter((edge) => edge.source === source);
+      const targetEdges = edges.filter((edge) => edge.target === target);
+
+      // Prevent more than one edge from the same source
+      if (sourceEdges.length > 0) {
+        return false;
+      }
+
+      // Allow multiple edges to the same target
+      return true;
+    },
+    [edges]
+  );
+
+  return (
+    <>
+      <Navbar saveEditedNode={saveEditedNode} />
+      <div className="dndflow flex flex-col md:flex-row flex-grow h-full">
+        <ReactFlowProvider>
+          <div
+            className="reactflow-wrapper flex-grow h-full"
+            ref={reactFlowWrapper}
+          >
+            <ReactFlow
+              nodes={nodes}
+              edges={edges}
+              onNodesChange={onNodesChange}
+              onEdgesChange={onEdgesChange}
+              onConnect={onConnect}
+              onInit={setReactFlowInstance}
+              onDrop={onDrop}
+              onDragOver={onDragOver}
+              onNodeClick={onNodeClick}
+              nodeTypes={nodeTypes}
+              fitViewOptions={{ maxZoom: 30 }}
+              isValidConnection={isValidConnection}
+            >
+              <Controls />
+            </ReactFlow>
+          </div>
+          <SidePanel selectedNode={selectedNode} handleEdit={handleEdit} />
+        </ReactFlowProvider>
+      </div>
+    </>
+  );
+};
+
+export default Flow;
